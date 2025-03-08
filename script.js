b27012881b30723afb53a49013eb9496d483d776
MathenL1 <lawmathen@gmail.com>
2025-03-08 21:14:25 +0000

    chrismas mathen

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function fix() {
-    //Write your code here!
+let coconut = document.getElementById("coconut");
+coconut.remove()
+let watermelon = document.getElementById("watermelon");
+watermelon.remove()
+let coast = document.getElementById("coast");
+coast.remove()
+let sunflowers = document.getElementById("sunflowers");
+sunflowers.remove()
+tree.id = "tree";
+let tree = document.getElementById("tree");
+let photos = document.getElementById("photos");
+tree.appendChild(tree);
 }
